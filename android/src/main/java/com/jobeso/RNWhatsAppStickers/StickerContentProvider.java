938f27f6228ff503fea2feefc8a97745c79226ee
wingskh <deathlwing@gmail.com>
2019-07-26 13:24:10 +0800

    change in to open

--- android/src/main/java/com/jobeso/RNWhatsAppStickers/StickerContentProvider.java
+++ android/src/main/java/com/jobeso/RNWhatsAppStickers/StickerContentProvider.java
@@ -259,7 +259,7 @@ public class StickerContentProvider extends ContentProvider {
         final FileOutputStream fileOutputStream = new FileOutputStream(file)) {
             byte[] buffer = new byte[1024];
             int read;
-            while ((read = in.read(buffer)) != -1) {
+            while ((read = open.read(buffer)) != -1) {
                 out.write(buffer, 0, read);
             }
         }
